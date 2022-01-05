053fc4b2571c2693272bc52ade83447507be7fdb
Rodrigoxalmeida <rodrigox.almeida@yahoo.com.br>
2022-01-04 21:40:16 -0300

    Projeto

---
+++ src/main/java/com/devsuperior/userdept/EventoApp1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.userdept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoApp1Application.class, args);
+	}
+
+}
